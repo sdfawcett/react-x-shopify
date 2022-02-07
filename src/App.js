6c50ef53b42860395bd6b0266bb3e08232663d2b
Shayne <shayne.fawcett@gmail.com>
2022-02-07 16:11:06 -0500

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar'
+
+import Home from './pages/Home'
+import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/products/:handle">
+            <ProductPage />
+          </Route>
+          <Route path="/">
+          <Home />
+          </Route>
+        </Switch>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Cart.js
@@ -0,0 +1,8 @@
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/shopContext'
+
+const Cart = () => {
+  return <div></div>;
+};
+
+export default Cart;
