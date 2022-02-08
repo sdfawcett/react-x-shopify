e9c8845de3e59132343baae499cb50740add5f12
Shayne <shayne.fawcett@gmail.com>
2022-02-08 12:47:21 -0500

    end of base app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import ProductPage from './pages/ProductPage';
 
+
 function App() {
   return (
     <div className="App">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { Box, Grid, Text, Image } from '@chakra-ui/react'
+import ImageWithText from '../components/ImageWithText';
+import Hero from '../components/Hero';
  
 import { ShopContext } from '../context/shopContext'
+import RichText from '../components/RichText';
 
 const Home = () => {
 
@@ -16,16 +19,21 @@ const Home = () => {
 
   return (
   <Box>
+      <Hero />
+      <RichText 
+        heading="The relaxation you've been waiting for."
+        text="Our Bath Bombs guarantee a fun, relaxing, and colorful night"
+      />
       <Grid templateColumns="repeat(3, 1fr)">
         {
             products.map(product => (
                 <Link to={`/products/${product.handle}`} key={product.id}>
-                    <Box _hover={{ opacity: '80%' }} textAlign="center">
+                    <Box _hover={{ opacity: '80%' }} textAlign="center" position="relative">
                         <Image src={product.images[0].src} />
-                        <Text>
+                        <Text position="absolute" bottom="15%" w="100%" fontWeight="bold">
                             {product.title}
                         </Text>
-                        <Text>
+                        <Text position="absolute" bottom="5%" w="100%" color="gray.500">
                             ${product.variants[0].price}
                         </Text>
                     </Box>
@@ -33,6 +41,20 @@ const Home = () => {
             ))
         }
       </Grid>
+      <RichText 
+        heading="Treat Yourself."
+      />
+      <ImageWithText 
+        image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758" 
+        heading="Heading" 
+        text="I'm baby kale chips twee skateboard tattooed, DIY iPhone ugh mixtape tumeric unicorn narwhal. Iceland shoreditch authentic, sartorial vegan twee flannel banh mi bushwick retro farm-to-table single-origin coffee."
+      />
+      <ImageWithText
+        reverse 
+        image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/bath-bomb-and-candle.jpg?v=1610066758" 
+        heading="Second Heading" 
+        text="I'm baby kale chips twee skateboard tattooed, DIY iPhone ugh mixtape tumeric unicorn narwhal. Iceland shoreditch authentic, sartorial vegan twee flannel banh mi bushwick retro farm-to-table single-origin coffee."
+      />
   </Box>
   )
 };
